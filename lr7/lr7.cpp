aa1814df0c6732c1c1793cfd14fc1e7099214768
olgarudkevich <olgarudkevich3785@gmail.com>
2025-10-06 14:40:34 +0300

    Add project files.

---
+++ lr7/lr7.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int n = 5;
+    int a[n][n];
+    int sum = 0;
+
+    cout << "Введіть елементи матриці 5x5:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i][j] > 0) {
+                sum += a[i][j];
+            }
+        }
+    }
+
+    cout << "Сума додатних елементів над головною діагоналлю = " << sum << endl;
+
+    return 0;
+}
